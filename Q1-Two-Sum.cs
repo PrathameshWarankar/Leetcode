8dcad10adb3685ec8d0c57bab610445538a17c80
Prathamesh Warankar <62240931+PrathameshWarankar@users.noreply.github.com>
2026-04-13 17:09:10 +0530

    Implement TwoSum method to find indices of two numbers

--- Q1-Two-Sum.cs
+++ Q1-Two-Sum.cs
@@ -1,1 +1,23 @@
+public class Solution 
+{
+    public int[] TwoSum(int[] nums, int target) 
+    {
+        var dict = new Dictionary<int, int>();
 
+        for(int i = 0; i <= nums.Length -1; i++)
+        {
+            var diff = target - nums[i];
+
+            if(dict.Keys.Contains(diff))
+            {
+                return new int[] {dict[diff], i};
+            }
+            else 
+            {
+                dict[nums[i]] = i;
+            }
+        }
+
+        return new int[] {-1, -1};
+    }
+}
