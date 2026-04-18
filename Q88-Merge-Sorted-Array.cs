17def2edfe0ebff749ba80d2c50c3dcc8fa8dc49
Prathamesh Warankar <62240931+PrathameshWarankar@users.noreply.github.com>
2026-04-18 21:45:37 +0530

    Implement merge function for two sorted arrays

---
+++ Q88-Merge-Sorted-Array.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public void Merge(int[] nums1, int m, int[] nums2, int n) {
+        var i = m - 1;
+        var j = n - 1;
+        var k = m + n - 1;
+
+        while (k >= 0)
+        {
+            if (i >= 0 && j >= 0)
+            {
+                if (nums1[i] > nums2[j])
+                {
+                    nums1[k] = nums1[i];
+                    i -= 1;
+                }
+                else 
+                {
+                    nums1[k] = nums2[j];
+                    j -= 1;
+                }
+            }
+            else if (i < 0)
+            {
+                nums1[k] = nums2[j];
+                j -= 1;
+            }
+            else
+            {
+                nums1[k] = nums1[i];
+                i -= 1;
+            }
+
+            k -= 1;
+        }
+    }
+}
