c19d297fee665e70069a9fc411d8b5d6f15b2e2e
Prathamesh Warankar <62240931+PrathameshWarankar@users.noreply.github.com>
2026-04-19 15:29:58 +0530

    Implement MaxProfit method for stock prices

---
+++ Q121-Best-Time-To-Buy-And-Sell-Stock.cs
@@ -0,0 +1,22 @@
+public class Solution 
+{
+    public int MaxProfit(int[] prices) 
+    {
+        int buyPrice = prices[0];
+        int maxProfit = 0;
+            
+        for (int i = 1; i < prices.Length; i++)
+        {
+            if (prices[i] - buyPrice > maxProfit)
+            {
+                maxProfit = prices[i] - buyPrice;
+            }
+            else if (prices[i] < buyPrice)
+            {
+                buyPrice = prices[i];
+            }
+        }
+
+        return maxProfit;
+    }
+}
